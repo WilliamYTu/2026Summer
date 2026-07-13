5f45e7417e7aaea1c01c3f8800b4d7351d5208c6
William <wyt4148.t@gmail.com>
2026-07-13 15:11:26 -0500

    deleted some code and wrote some backend

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.kt
@@ -2,9 +2,25 @@ package org.firstinspires.ftc.teamcode
 import com.bylazar.configurables.annotations.Configurable
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.Limelight
+
 
 @Configurable
 class Robot( val hardwareMap: HardwareMap, val telemetry: Telemetry) {
     val limelight = Limelight(hardwareMap, telemetry)
+    // default pipeline is 1 not 0 for this config
+    fun initLimelight(){
+        limelight.initLimelight()
+    }
+    fun limelightToggle(){
+        if (limelight.state == Limelight.LimelightState.OFF){
+            limelight.start()
+        } else{
+            limelight.stop()
+        }
+    }
+
+    fun switchPipeline(pipeline: Int){
+        limelight.switchPipeline(pipeline)
+    }
+
 }
